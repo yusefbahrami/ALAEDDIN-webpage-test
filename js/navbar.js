1634b44dd2efc8400536ad4ad62f2aadbd0f3554
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-11-19 02:16:17 +0330

    navbar

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,6 @@ const navBar = document.querySelector(".nav-bar");
 const humburgerMenuButton = document.querySelector(".humburger");
 function toggle() {
   navBar.classList.toggle("show-menu");
-  humburgerMenuButton.style.display = "block";
   if (navBar.classList.contains("show-menu")) {
     humburgerMenuButton.style.display = "none";
   }
