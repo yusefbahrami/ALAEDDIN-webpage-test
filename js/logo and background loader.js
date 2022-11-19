650c540408edda8cd3116b0a21b40c32d839ace5
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-11-19 22:45:34 +0330

    images load with fetch api

---
+++ js/logo and background loader.js
@@ -0,0 +1,14 @@
+const imgLogo = document.querySelector(".img-logo");
+const imgProfile = document.querySelector(".profile-image");
+window.onload = async () => {
+  const result = await fetch("./assets/IMG_20221024_160602_501.jpg");
+  console.log(result);
+  console.log(imgLogo);
+  console.log(imgProfile);
+  if (result.status == 200) {
+    imgLogo.setAttribute("src", result.url);
+    imgProfile.setAttribute("herf", result.url);
+    document.body.style.background = `linear-gradient(rgba(13, 71, 83, 78%), rgba(13, 71, 83, 78%)),
+    url("./assets/IMG_20221024_160602_501.jpg") center center/cover fixed`;
+  }
+};
