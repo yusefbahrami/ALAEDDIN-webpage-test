cf60f6c9d53d7900dbcb6ff12a315cc15f7941c8
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-11-19 23:25:49 +0330

    the bug of the image link was fixed

--- js/logo and background loader.js
+++ js/logo and background loader.js
@@ -4,7 +4,7 @@ window.onload = async () => {
   const result = await fetch("./assets/IMG_20221024_160602_501.jpg");
   if (result.status == 200) {
     imgLogo.setAttribute("src", result.url);
-    imgProfile.setAttribute("herf", result.url);
+    imgProfile.setAttribute("href", result.url);
     document.body.style.background = `linear-gradient(rgba(13, 71, 83, 78%), rgba(13, 71, 83, 78%)),
     url("./assets/IMG_20221024_160602_501.jpg") center center/cover fixed`;
   }
